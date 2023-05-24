b511514e27226ab7e9edd1d201cc2c5c815205c7
Artur <ilyachever@gmail.com>
2023-05-24 19:59:19 +0300

    Added additional mock-data to server.js

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -30,12 +30,32 @@ const data = {
             title: 'Павлин',
             time: '14.04.2023',
         },
+        {
+            src: './images/birds/parrot.webp',
+            title: 'Попугай малазийский',
+            time: '15.04.2023',
+        },
+        {
+            src: './images/birds/mandarin.webp',
+            title: 'Мандаринка',
+            time: '17.04.2023',
+        },
+        {
+            src: './images/birds/parrotRed.webp',
+            title: 'Попугай бразильский',
+            time: '18.04.2023',
+        },
+        {
+            src: './images/birds/bunting.webp',
+            title: 'Горная овсянка',
+            time: '19.04.2023',
+        },
     ],
     wild: [
         {
             src: './images/birds/eagle.webp',
             title: 'Орёл',
-            time: '14.04.2023',
+            time: '20.04.2023',
         },
         {
             src: './images/birds/falcon.webp',
@@ -61,6 +81,36 @@ const data = {
             src: './images/birds/swift.webp',
             title: 'Стриж',
             time: '10.05.2023',
+        },
+        {
+            src: './images/birds/eagle.webp',
+            title: 'Орёл',
+            time: '02.06.2023',
+        },
+        {
+            src: './images/birds/falcon.webp',
+            title: 'Сапсан',
+            time: '05.06.2023',
+        },
+        {
+            src: './images/birds/osprey.webp',
+            title: 'Скопа',
+            time: '07.06.2023',
+        },
+        {
+            src: './images/birds/owl.webp',
+            title: 'Сова',
+            time: '10.06.2023',
+        },
+        {
+            src: './images/birds/seagull.webp',
+            title: 'Чайка',
+            time: '12.06.2023',
+        },
+        {
+            src: './images/birds/swift.webp',
+            title: 'Стриж',
+            time: '15.06.2023',
         },
     ],
     pet: [
@@ -93,6 +143,66 @@ const data = {
             src: './images/birds/pheasant.webp',
             title: 'Фазан',
             time: '31.05.2023',
+        },
+        {
+            src: './images/birds/duck.webp',
+            title: 'Утка',
+            time: '01.07.2023',
+        },
+        {
+            src: './images/birds/goose.webp',
+            title: 'Гусь',
+            time: '03.07.2023',
+        },
+        {
+            src: './images/birds/hen.webp',
+            title: 'Петух',
+            time: '05.07.2023',
+        },
+        {
+            src: './images/birds/turkey.webp',
+            title: 'Индюшка',
+            time: '06.07.2023',
+        },
+        {
+            src: './images/birds/swan.webp',
+            title: 'Лебедь',
+            time: '12.07.2023',
+        },
+        {
+            src: './images/birds/pheasant.webp',
+            title: 'Фазан',
+            time: '14.07.2023',
+        },
+        {
+            src: './images/birds/duck.webp',
+            title: 'Утка',
+            time: '16.07.2023',
+        },
+        {
+            src: './images/birds/goose.webp',
+            title: 'Гусь',
+            time: '02.08.2023',
+        },
+        {
+            src: './images/birds/hen.webp',
+            title: 'Петух',
+            time: '08.08.2023',
+        },
+        {
+            src: './images/birds/turkey.webp',
+            title: 'Индюшка',
+            time: '16.08.2023',
+        },
+        {
+            src: './images/birds/swan.webp',
+            title: 'Лебедь',
+            time: '24.08.2023',
+        },
+        {
+            src: './images/birds/pheasant.webp',
+            title: 'Фазан',
+            time: '30.09.2023',
         },
     ],
 }
