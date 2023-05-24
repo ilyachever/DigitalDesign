d1d3e09560842bf096935c712fdb55121985c5f7
Artur <ilyachever@gmail.com>
2023-05-24 19:48:55 +0300

    Refactor getDate method view's object

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -6,8 +6,89 @@ const view = {
         contentContainer.innerHTML = "";
     },
     // getDate - преобразование входящих данных о времени публикации в человекочитаемый вид.
-    getDate(string) {
-        return string;
+    getDate(time) {
+        const dateComponents = time.split('.');
+        const [dateDay, dateMonth, dateYear] = dateComponents;
+
+        const date = new Date(`${dateYear}.${dateMonth}.${dateDay}`);
+
+        const currentDay = date.getDay();
+        const currentDayOfMonth = date.getDate();
+        const currentMonth = date.getMonth();
+        const currentYear = date.getFullYear();
+
+        // Возвращает день недели в зависимости от его номера.
+        function getDay(day = currentDay) {
+            switch (day) {
+                case 0:
+                    return 'Воскресенье';
+                case 1:
+                    return 'Понедельник';
+                case 2:
+                    return 'Вторник';
+                case 3:
+                    return 'Среда';
+                case 4:
+                    return 'Четверг';
+                case 5:
+                    return 'Пятница';
+                case 6:
+                    return 'Суббота';
+                default:
+                    return 'Неизвестный день недели';
+            }
+        }
+        // Возвращает номер недели в зависимости от числа месяца.
+        function getWeek(dayOfMonth = currentDayOfMonth) {
+            if (dayOfMonth >= 1 && dayOfMonth <= 7) {
+                return '1 неделя';
+            } else if (dayOfMonth >= 8 && dayOfMonth <= 14) {
+                return '2 неделя';
+            } else if (dayOfMonth >= 15 && dayOfMonth <= 21) {
+                return '3 неделя';
+            } else if (dayOfMonth >= 22 && dayOfMonth <= 28) {
+                return '4 неделя';
+            } else {
+                return '5 неделя';
+            }
+        }
+        // Возвращает месяц в зависимости от его номера.
+        function getMonth(month = currentMonth) {
+            switch (month) {
+                case 0:
+                    return 'Января';
+                case 1:
+                    return 'Февраля';
+                case 2:
+                    return 'Марта';
+                case 3:
+                    return 'Апреля';
+                case 4:
+                    return 'Мая';
+                case 5:
+                    return 'Июня';
+                case 6:
+                    return 'Июля';
+                case 7:
+                    return 'Августа';
+                case 8:
+                    return 'Сентября';
+                case 9:
+                    return 'Октября';
+                case 10:
+                    return 'Ноября';
+                case 11:
+                    return 'Декабря';
+                default:
+                    return 'Неизвестный месяц';
+            }
+        }
+        // Возвращает текущий год.
+        function getYear(year = currentYear) {
+            return `${year} года`
+        }
+
+        return `${getDay()}, ${getWeek()} ${getMonth()} ${getYear()}`;
     },
     // renderTemplate - рендер HTML шаблона для продукта.
     renderTemplate(src, title, time) {
