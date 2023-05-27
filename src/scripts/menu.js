5114b5f7d7583c62651aabf94ae3f95af1cdffa0
Artur <ilyachever@gmail.com>
2023-05-27 14:24:22 +0300

    Refactor, connected elements directly to listeners

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,12 +1,13 @@
 // HTML Элементы
 const nav = document.querySelector('.header__navigation');
+const menuButton = document.querySelector('.header__menu');
 
 // Если JS не загрузится, навигация будет доступна благодаря модификатору класса "--nojs"
 function hideNavigation() {
     nav.classList.remove('header__navigation--nojs');
 }
 
-// Функция открыть/закрыть меню
+// Функция открыть/закрыть меню при нажатии на кнопку.
 function handlerClick() {
     const iconAttributes = {
         open: './images/sprite/sprite.svg#menu',
@@ -16,11 +17,11 @@ function handlerClick() {
     nav.classList.toggle('header__navigation--opened');
 
     if(nav.classList.contains('header__navigation--opened')) {
-        changeIcon(this, iconAttributes.close)
-        this.setAttribute('aria-label', 'Закрыть навигацию сайта.')
+        changeIcon(menuButton, iconAttributes.close);
+        menuButton.setAttribute('aria-label', 'Закрыть навигацию сайта.');
     } else {
-        changeIcon(this, iconAttributes.open)
-        this.setAttribute('aria-label', 'Открыть навигацию сайта.')
+        changeIcon(menuButton, iconAttributes.open);
+        menuButton.setAttribute('aria-label', 'Открыть навигацию сайта.');
     }
 }
 
@@ -28,16 +29,10 @@ function handlerClick() {
 function changeIcon(parentElement, attribute) {
     const iconContainer = parentElement.querySelector('use');
 
-    iconContainer.setAttribute('href', attribute)
-}
-
-// Функция закрытия навигации.
-function closeNavigation() {
-    nav.classList.remove('header__navigation--opened');
+    iconContainer.setAttribute('href', attribute);
 }
 
 export {
     hideNavigation,
-    closeNavigation,
     handlerClick
 }
